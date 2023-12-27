062597a827c56e691aecdde283c9ebb2146be6da
Denis Makukh <d.makuh@ntc-vulkan.ru>
2023-12-27 14:46:22 +0300

    producer implementation

---
+++ src/test/java/com/makukh/springboot/SpringbootKafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makukh.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
